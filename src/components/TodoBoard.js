889405ea46c494ab0a2e636b77a54c87a0668740
tjwls11 <166362199+tjwls11@users.noreply.github.com>
2024-07-04 18:37:26 +0900

    Update TodoBoard.js

--- src/components/TodoBoard.js
+++ src/components/TodoBoard.js
@@ -1,15 +1,28 @@
-import React from "react";
-import TodoItem from "./TodoItem";
+import React from 'react';
+import TodoItem from './TodoItem';
 
-function TodoBoard(props){
-    console.log("todoBoard", props.todoList);
+function TodoBoard({ todoList, onDelete, onUpdate }) {
+    const handleUpdateItem = (index, updatedItem) => {
+        const updatedList = [...todoList];
+        updatedList[index] = updatedItem;
+        onUpdate(updatedList);
+    };
 
     return (
-        <div>
-            <h1>My Todo List</h1>
-            {props.todoList.map((item, index) => (
-                <TodoItem key={index} item={item} onDelete={() => props.onDelete(index)} />
-            ))}
+        <div className="todo-board">
+            <h1 style={{ color: '#974D99' }}>My Todo List</h1>
+            {todoList.length === 0 ? (
+                <p>No items in the list</p>
+            ) : (
+                todoList.map((item, index) => (
+                    <TodoItem
+                        key={index}
+                        item={item}
+                        onDelete={() => onDelete(index)}
+                        onUpdate={(updatedItem) => handleUpdateItem(index, updatedItem)}
+                    />
+                ))
+            )}
         </div>
     );
 }
